056bbb64a2d9540560c511e748cde96ae9f52f80
Franco Sanchez Alejandro <francosanche@gmail.com>
2022-06-05 17:01:42 -0300

    second try, not completed

--- time_calculator.py
+++ time_calculator.py
@@ -15,6 +15,7 @@ def add_time(start: str, duration: str, day: str = None):
     # start[0] hours
     # start[1] min
     # start[2] AM-PM
+    print(start[2])
 
     duration = duration.split(":")
     # duration[0] = hours
@@ -28,59 +29,15 @@ def add_time(start: str, duration: str, day: str = None):
     return get_next_time(start, duration, day)
 
 
-def return_day_am_or_pm(am_or_pm: str, twelve_hours_passed: int):
-    result = ["", ""]
-    total_days = 0
-    if twelve_hours_passed % 2 == 1:
-        if am_or_pm == "AM":
-            result[0] = "PM"
-        else:
-            total_days = total_days + 1
-            result[0] = "AM"
-    else:
-        result[0] = am_or_pm
-
-    while twelve_hours_passed >1:
-        twelve_hours_passed = twelve_hours_passed - 2
-        total_days = total_days + 1
-
-    result[1] = total_days
-
-    return result
-
-
-def get_day(days_to_change: int, day: str = None):
-
-    result = ["", ""]
-
-    extra_space = ""
-
-    if days_to_change == 1:
-        result[0] = " (next day)"
-    elif days_to_change == 0:
-        extra_space = ""
-        result[0] = ""
-    else:
-        result[0] = f" ({days_to_change} days later)"
-
-    if day:
-        days_to_change = DaysOfTheWeek[day.capitalize()].value + days_to_change
-        while days_to_change >= 7:
-            days_to_change = days_to_change - 7
-        if days_to_change == 0:
-            result[1] = f", {day.capitalize()}{extra_space}"
-        else:
-            result[1] = f", {DaysOfTheWeek(days_to_change).name}{extra_space}"
-
-
-
-    return f"{result[1]}{result[0]}"
-
 
 def get_next_time(start: list, duration: list, day: str = None):
+
+    if start[2] == "PM":
+        hours_summed = int(duration[0]) + int(start[0]) + 12
+
     minutes_summed = int(duration[1]) + int(start[1])
     hours_summed = int(duration[0]) + int(start[0])
-    twelve_hours_summed = 0
+
     total_days_summed = 0
 
     result = ["", "", "", ""]
@@ -93,35 +50,93 @@ def get_next_time(start: list, duration: list, day: str = None):
         minutes_summed = minutes_summed - 60
         hours_summed = hours_summed + 1
 
-    while hours_summed > 11:
-        hours_summed = hours_summed - 12
-        twelve_hours_summed = twelve_hours_summed + 1
+    while hours_summed > 23:
+        hours_summed = hours_summed - 24
+        total_days_summed = total_days_summed + 1
 
-    # while twelve_hours_summed > 1:
-    #     twelve_hours_summed = twelve_hours_summed - 2
-    #     total_days_summed = total_days_summed + 1
 
     # -------- crating the result --------
 
+
+
+    if minutes_summed <= 9:
+        minutes_summed = f"0{minutes_summed}"
+
+    result[1] = minutes_summed
+
+    if hours_summed > 12:
+        hours_summed = hours_summed - 12
+        result[2] = change_am_or_pm(start[2])
+    else:
+        result[2] = start[2]
+
     result[0] = hours_summed
 
-    if minutes_summed < 10:
-        result[1] = f"0{minutes_summed}"
-    else:
-        result[1] = minutes_summed
-
-    result[2], result[3] = return_day_am_or_pm(start[2], twelve_hours_summed)
-
-    result[3] = get_day(result[3], day)
+    result[3] = returning_day_result(total_days_summed, day)
 
     return f"{result[0]}:{result[1]} {result[2]}{result[3]}"
 
 
+
+
 class DaysOfTheWeek(Enum):
-    Monday = 1
-    Tuesday = 2
-    Wednesday = 3
-    Thursday = 4
-    Friday = 5
-    Saturday = 6
-    Sunday = 7
+    Monday = 0
+    Tuesday = 1
+    Wednesday = 2
+    Thursday = 3
+    Friday = 4
+    Saturday = 5
+    Sunday = 6
+
+def get_next(day):
+    if type(day) == int:
+        next_day = day + 1
+        if next_day >= 7:
+            return DaysOfTheWeek.Monday.name
+        else:
+            return DaysOfTheWeek(day  + 1).name
+    elif type(day) == str:
+        get_next(DaysOfTheWeek[day.capitalize()].value)
+
+
+def change_am_or_pm(am_or_pm: str):
+    if am_or_pm.upper() == "AM":
+        return "PM"
+    else:
+        return "AM"
+
+def get_current_day(days_passed: int, day: str):
+    for day_passed in range(days_passed):
+        day = get_next(day)
+    return day
+
+def returning_day_result(days_passed: int, day: str):
+    result_1 = ""
+    result_2 = ""
+    # handles the none value
+    if day:
+        result_1 = f", {get_current_day(days_passed, day)}"
+    if days_passed == 0:
+        pass
+    elif days_passed == 1:
+        result_2 = " (next day)"
+    else:
+        result_2 = f" ({days_passed} days later)"
+
+    return result_1 + result_2
+
+def day_of_duration(duration: list, caca: list):
+
+    mins = int(duration[1]) + int(caca[1])
+    hours = (int(duration[0])) + (int(caca[0]))
+    days = 0
+
+    while mins > 59 :
+        mins = mins - 60
+        hours = hours + 1
+    while hours > 23 :
+        hours = hours - 24
+        days = days + 1
+
+
+    return f"[added: {duration}, current: {caca},result = [{hours}:{mins}, days: {days}]]"
